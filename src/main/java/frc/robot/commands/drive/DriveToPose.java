63729f366314236ec2339ccca83391bc95f00857
Alex Clute <55024407+Robotic-Racer14a@users.noreply.github.com>
2025-12-10 09:38:58 -0500

    Changed the direction it heads in and added comments

--- src/main/java/frc/robot/commands/drive/DriveToPose.java
+++ src/main/java/frc/robot/commands/drive/DriveToPose.java
@@ -4,6 +4,8 @@ import com.ctre.phoenix6.swerve.utility.PhoenixPIDController;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.IntermediatePoseObject;
@@ -18,6 +20,7 @@ public class DriveToPose extends Command {
     private int stepsLeft;
     private final int numberOfSteps;
     private double distanceUntilContinue = 0;
+    private double followingDistance = 0.5;
 
     private final PIDController translationalController = new PIDController(0.001, 0, 0);
     private final PhoenixPIDController rotationalController = new PhoenixPIDController(0.001, 0, 0);
@@ -52,9 +55,15 @@ public class DriveToPose extends Command {
         double xPow = 0, yPow = 0;
         Pose2d currentPose = drive.getCurrentPose();
         
+        //Determines target pose and when to move to the next pose
         if (stepsLeft == numberOfSteps) {
             targetPose = endPose;
         } else {
+            //This is done by figuring out the intersection of two tangent lines on a circle
+            //Because we know that the Hypotenuse is the same for both triangles, and the radius is one side of the triangle, we know that both triangles are identical
+            //This allows us to determine the other side, which is our distance away from intersection point to where we move on to next line
+            //Ask Alex Clute if you have any questions
+
             targetPose = intermediatePoses[numberOfSteps - stepsLeft].getIntermediatePose();
             Pose2d nextPose = stepsLeft == 1 ? endPose : intermediatePoses[numberOfSteps - stepsLeft - 1].getIntermediatePose();
             double angleToPose = angleBetweenPoses(targetPose, currentPose);
@@ -64,20 +73,36 @@ public class DriveToPose extends Command {
             distanceUntilContinue = intermediatePoses[numberOfSteps - stepsLeft].getRadius() / Math.tan(angleBetweenPoses / 2);
         }
 
+        //Determines the speed the robot should be driving with
+        //For Continuous moves, we don't want it to slow down, just affect direction
+        //For fine moves, we want it to stop at our desired location
         double distanceAway = intermediatePoses[numberOfSteps - stepsLeft].getRadius() == 0 ? distanceBetweenPoses(currentPose, targetPose) : distanceBetweenPoses(currentPose, endPose);
         double translationOutput = Math.min(translationalController.calculate(distanceAway, 0), 1);
+
+        //Determines the distance from target we should be driving towards
+        followingDistance = Math.max(distanceAway - followingDistance, 0);
+
+        //Determine targetPose with a rotation of the slope between points
+        Pose2d previousPose = stepsLeft + 1 > numberOfSteps ? currentPose : intermediatePoses[numberOfSteps - stepsLeft - 1].getIntermediatePose();
+        Pose2d angleToPose = new Pose2d(targetPose.getX(), targetPose.getY(), Rotation2d.fromRadians(angleBetweenPoses(previousPose, targetPose)));
+
+        //Offset angleToPose to angle towards correct spot on line, and fix angle to be target angle
+        angleToPose.transformBy(new Transform2d(0, followingDistance, Rotation2d.kZero));
+        angleToPose = new Pose2d(angleToPose.getX(), angleToPose.getY(), targetPose.getRotation());
         
-        double angleOfDistance = angleBetweenPoses(currentPose, targetPose);
+        //Applies power in the direction set by the angle pose
+        double angleOfDistance = angleBetweenPoses(currentPose, angleToPose);
         xPow = translationOutput * Math.cos(angleOfDistance);
         yPow = translationOutput * Math.sin(angleOfDistance);
 
-        
+        //Sends power to chassis
         drive.setControl(
             drive.facingAngle.withVelocityX(xPow * drive.MaxSpeed)
             .withVelocityY(yPow * drive.MaxSpeed)
             .withTargetDirection(endPose.getRotation())
             );
 
+        //Moves on to the next step once step is complete
         if (distanceUntilContinue > distanceBetweenPoses(currentPose, targetPose) - translationalController.getErrorTolerance() && !(stepsLeft == numberOfSteps)) {
             stepsLeft--;
         }
