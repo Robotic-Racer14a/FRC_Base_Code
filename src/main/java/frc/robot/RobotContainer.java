ff5673637c91fa20bf4826b395884186276a5115
Alex Clute <55024407+Robotic-Racer14a@users.noreply.github.com>
2026-01-07 13:21:20 -0500

    added joystick command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -18,6 +18,8 @@ import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.commands.drive.DriveToPose;
 import frc.robot.commands.drive.FieldCentricControl;
+import frc.robot.commands.elevator.ElevatorJoystick;
+import frc.robot.commands.elevator.ElevatorPID;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.ElevatorSubsystem;
@@ -63,10 +65,14 @@ public class RobotContainer {
 
         drive.setDefaultCommand(new FieldCentricControl(drive, driverController));
 
+        elevator.setDefaultCommand(new ElevatorPID(elevator));
+
         driverController.a().onTrue(new InstantCommand(() -> drive.setUseMT1(true)));
         driverController.b().onTrue(new InstantCommand(() -> drive.setUseMT2(true)));
         driverController.x().onTrue(new InstantCommand(() -> drive.setUseMT1(false)));
         driverController.x().onTrue(new InstantCommand(() -> drive.setUseMT2(false)));
+
+        operatorController.a().whileTrue(new ElevatorJoystick(elevator, () -> operatorController.getLeftY()));
         
         drive.registerTelemetry(logger::telemeterize);
     }
---
+++ src/main/java/frc/robot/commands/elevator/ElevatorJoystick.java
@@ -0,0 +1,39 @@
+package frc.robot.commands.elevator;
+
+import java.util.function.Supplier;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.SystemVariables;
+import frc.robot.subsystems.ElevatorSubsystem;
+
+public class ElevatorJoystick extends Command{
+
+    private ElevatorSubsystem elevator;
+    private Supplier<Double> power;
+
+    public ElevatorJoystick (ElevatorSubsystem elevator, Supplier<Double> power) {
+        this.elevator = elevator;
+        this.power = power;
+        addRequirements(elevator);
+    }
+    
+    @Override
+    public void initialize() {
+
+    }
+    
+    @Override
+    public void execute() {
+        elevator.setElevatorPower(power.get());
+    }
+    
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+    
+    @Override
+    public void end(boolean finished) {
+        elevator.setElevatorPower(0);
+    }
+}
